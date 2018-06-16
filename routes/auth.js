47c8f988ac8cc2bd985660d8bba50d3394bb362a
Alexander Spieslechner <aspieslechner@AT03MN00002.local>
2018-06-16 14:16:35 +0200

    allow registration

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('./../controllers/authController');
+const userController = require('./../controllers/userController');
 
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
+router.post('/register', userController.create);
 
 module.exports = router;
